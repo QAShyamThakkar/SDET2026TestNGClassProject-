361f6c59f2cd6f1bdf445141d0488eb025f01427
QAShyamThakkar <khimanishyam@gmail.com>
2026-04-16 07:59:34 +0530

    updated up to class70

--- src/test/java/Class67_TestNGClass1/BaseTest.java
+++ src/test/java/Class67_TestNGClass1/BaseTest.java
@@ -1,17 +1,33 @@
 package Class67_TestNGClass1;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.*;
 
 public class BaseTest {
+    WebDriver driver;
+// String browser = "Chrome";
 
+    @Parameters("browser")
     @BeforeTest
-    public void setUp(){
+    public void setUp(String browser) {
+
+        if (browser.equalsIgnoreCase("Chrome")) {
+            driver = new ChromeDriver();
+        } else {
+            driver = new FirefoxDriver();
+        }
+
+
         System.out.println("0Opening the Website");
     }
 
 
     @AfterTest
-    public void cleanUp(){
+    public void cleanUp() {
         System.out.println("9Closing the Website");
     }
 
--- src/test/java/Class67_TestNGClass1/LoginTest.java
+++ src/test/java/Class67_TestNGClass1/LoginTest.java
@@ -5,17 +5,17 @@ import org.testng.annotations.*;
 public class LoginTest extends BaseTest{
 
 
-    @Test (groups = "smoke")
+    @Test (groups = "smoke", priority = 1, dependsOnMethods = "verifyLogInUnsuccessful")
     public void verifyLogInSuccessful() {
         System.out.println("1_Test - Verify Log in is Successful");
     }
 
-    @Test (priority = 1)
+    @Test (priority = 2)
     public void verifyLogInUnsuccessful() {
         System.out.println("2_Test - Verify Log in is Unsuccessful");
     }
 
-    @Test (priority = -1)
+    @Test (priority = 3)
     public void verifyLogInWithBlankCred() {
         System.out.println("3_Test - Verify Log in is Unsuccessful with Blank Cred");
     }
